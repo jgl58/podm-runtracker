ab68334dc58eb04ee680aa5db3b262a8b26bd5f7
Jonay Gilabert López <jgl58@alu.ua.es>
2020-02-12 18:35:04 +0100

    Primer commit

---
+++ RunTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RunTracker
+//
+//  Created by Jonay Gilabert López on 12/02/2020.
+//  Copyright © 2020 Jonay Gilabert López. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
