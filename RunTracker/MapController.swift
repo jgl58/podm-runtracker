36f263e4350cbe3ff5400ee583de895497ebf5e2
Jonay Gilabert López <jgl58@alu.ua.es>
2020-02-12 19:26:07 +0100

    Boton play

--- RunTracker/MapController.swift
+++ RunTracker/MapController.swift
@@ -18,7 +18,8 @@
       
   {
       @IBOutlet weak var bigMap: MKMapView!
-      
+      @IBOutlet weak var btn: UIButton!
+
       let locationManager = CLLocationManager()
       var pins = [mapPin]()
       
@@ -32,17 +33,6 @@
           self.bigMap.showsUserLocation = true
           
           
-          /// DESPLAZARSE A ESPAÑA PARA VER LOS PINS
-          pins.append(mapPin("Compás Musical (Elche)", "Carrer Ruperto Chapí, 51, 03201 Elx, Alicante", [38.268247, -0.703280]))
-          pins.append(mapPin("Valencia Musical (Valencia)", "Carrer d'Àngel Guimerà, 11, 46008 València, Valencia", [39.470174, -0.385633]))
-          pins.append(mapPin("Instrumentos Musicales Musical 72 (Madrid)", "Calle de la Independencia, 3, 28013 Madrid", [40.417725, -3.709789]))
-          pins.append(mapPin("Tempo Instruments (Ontinyent)", "Av. d'Albaida, 12, 46870 Ontinyent, Valencia", [38.823880, -0.599586]))
-          pins.append(mapPin("GuitarShop Barcelona (Barcelona)", "Carrer dels Tallers, 27, 08001 Barcelona", [41.384768, 2.168808]))
-          pins.append(mapPin("Musical Guima (Granada)", "Camino de Ronda, 71, 18004 Granada", [37.170576, -3.606482]))
-          pins.append(mapPin("Musica Bilbao (Bilbao)", "Urkixo Zumarkalea, 92, 48013 Bilbo, Bizkaia", [43.262189, -2.945567]))
-          pins.append(mapPin("Rockbox (La Coruña)", "Rúa José Luis Pérez Cepeda, 26, 15004 A Coruña", [43.365512, -8.413180]))
-          pins.append(mapPin("Alteisa Sonido Salamanca (Salamanca)", "Calle Azafranal, 44, 37001 Salamanca", [40.967562, -5.659875]))
-          pins.append(mapPin("Musical Barragán (Cáceres)", "Calle Colón, 12, 10002 Cáceres", [39.469845, -6.373455]))
           
           var annotation = MKPointAnnotation()
           for pin in pins {
@@ -52,10 +42,26 @@
               annotation.coordinate = CLLocationCoordinate2D(latitude: pin.coordinates[0], longitude: pin.coordinates[1])
               bigMap.addAnnotation(annotation)
           }
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector (tap))  //Tap function will call when user tap on button
+        let longGesture = UILongPressGestureRecognizer(target: self, action: #selector(long))  //Long function will call when user long press on button.
+          tapGesture.numberOfTapsRequired = 1
+          btn.addGestureRecognizer(tapGesture)
+          btn.addGestureRecognizer(longGesture)
           
       
           
       }
+    
+    @objc func tap() {
+
+        print("Tap happend")
+    }
+
+    @objc func long() {
+
+        print("Long press")
+    }
       
       func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
           guard annotation is MKPointAnnotation else { return nil }
