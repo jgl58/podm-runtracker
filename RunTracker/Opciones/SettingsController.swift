93b1b93b26401e6389b3761322bd73e77bc0e5cb
Flaviu Lucian Georgiu <flaviu.georgiu97@gmail.com>
2020-02-26 19:06:24 +0100

    opciones básicas 2

--- RunTracker/Opciones/SettingsController.swift
+++ RunTracker/Opciones/SettingsController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Jonay Gilabert López. All rights reserved.
 //
 
+// IMPORTANTE!: NO BORRAR ESTE ARCHIVO, YA QUE ES EL ENCARGADO DE PINTAR LOS AJUSTES
 import Foundation
 
 class SettigsController : IASKAppSettingsViewController{
