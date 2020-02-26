f6e522a14a69d88d4f2cdb73e5278a1c01e4bd20
Flaviu Lucian Georgiu <flaviu.georgiu97@gmail.com>
2020-02-26 16:49:35 +0100

    Pods inicializados

---
+++ RunTracker/Opciones/Options-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Options-Bridging-Header.h
+//  RunTracker
+//
+//  Created by Máster Móviles on 26/02/2020.
+//  Copyright © 2020 Jonay Gilabert López. All rights reserved.
+//
+
+#ifndef Options_Bridging_Header_h
+#define Options_Bridging_Header_h
+
+#import "InAppSettingsKit/IASKAppSettingsViewController.h"
+
+#endif /* Options_Bridging_Header_h */
---
+++ RunTracker/Opciones/SettingsController.swift
@@ -0,0 +1,13 @@
+//
+//  IASKAppSettingsViewController.swift
+//  RunTracker
+//
+//  Created by Máster Móviles on 26/02/2020.
+//  Copyright © 2020 Jonay Gilabert López. All rights reserved.
+//
+
+import Foundation
+
+class SettigsController : IASKAppSettingsViewController{
+    
+}
